f10b6c499ebb86866dc143a487d5c694b8ace3dd
cprada33 <cprada33@hotmail.com>
2025-07-21 11:18:50 -0500

    base

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      color="primary"
+      dark
+      app
+    >
+      <v-app-bar-title>ClinCorp</v-app-bar-title>
+      
+      <v-spacer />
+      
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      color="primary"
+      dark
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }} ClinCorp - Sistema Clínico Corporativo</span>
+    </v-footer>
+  </v-app>
+</template>
